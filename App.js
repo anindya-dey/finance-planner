567bf58a798d152140bb1e83cb2530f3a0002289
Anindya Dey <andy.ani1992@gmail.com>
2023-09-11 23:51:05 +0530

    init

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Welcome to Finance Planner!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    color: 'teal',
+    fontSize: 30,
+    fontWeight: '200',
+  }
+});
